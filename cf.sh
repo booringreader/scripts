df36d9c0236f911404113b718cf8eff14f056f06
booringreader <157519312+booringreader@users.noreply.github.com>
2024-12-18 19:56:33 +0530

    fet: scripts added for boilerplate code on two platforms

---
+++ cf.sh
@@ -0,0 +1,13 @@
+echo "#include<bits/stdc++.h>
+#define ll long long
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+
+int main(){
+    FAST_IO;
+
+}
+" > $1.cpp
+
+# open file after it has been created 
+vim $1.cpp
